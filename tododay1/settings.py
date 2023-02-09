8311257ac9f21345e0609e2b1a9b1518a7e2467b
Itshari891 <kphari891@gmail.com>
2023-02-09 23:27:20 +0530

    todo

--- tododay1/settings.py
+++ tododay1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^ro^0($@c!x5lul53x&vrs3o$w8c-jp19ej##j%22h+nke9r@f'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
